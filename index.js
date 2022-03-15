ea76a2a7499bc4597aa7762a6ffc48f5354cfc49
yjoonjang <yjoonjang34@gmail.com>
2022-03-16 03:47:46 +0900

    first-commit

--- index.js
+++ index.js
@@ -15,82 +15,20 @@ const { json } = require('body-parser');
 const e = require('express');
 const { request, application } = require('express');
 
-app.get('/', (req, res) => {
-    res.send('hello world!');
-    return;
-});
-
-app.get('/user', (req, res) => {
-    // const { id } = base64.decode(req.body.id);
-    const id = req.body;
-    connection.query(`SELECT * FROM user WHERE user_id = '${id}'`, (error, results) => {
-        if (error) {
-            res.status(500).json(error);
-            return;
-        }
-        res.status(200).json(results);
-    });
-});
-// and ,
-app.post('/auth', (req, res) => {
-    const { id, password } = req.body;
-    connection.query(`SELECT * FROM user WHERE user_id = '${id}'`, function (error, results) {
-        if (results.length == 0) {
-            res.status(403).json({
-                code: 1,
-                message: '아이디를 확인해 주세요.',
-            });
-            return;
-        }
-        connection.query(
-            `SELECT * FROM user WHERE user_id = '${id}' and password = SHA2('${password}',512)`,
-            function (error, results) {
-                if (results.length == 0) {
-                    res.status(403).json({
-                        code: 2,
-                        message: '비밀번호를 확인해 주세요.',
-                    });
-                    return;
-                }
-                connection.query(
-                    `INSERT INTO LoginHistory(user_id,nickname,created_at) VALUES('${id}','${results[0].nickname}', NOW())`,
-                    function (error, results) {
-                        if (error) {
-                            console.log(error);
-                            return;
-                        }
-                    }
-                );
-                res.status(200).json(results);
-            }
-        );
-    });
-});
-
+// 회원가입
 app.post('/join', (req, res) => {
-    const { id, password, nickname } = req.body;
-    // const password = sha512(req.body.password);
-    console.log(password);
+    const { user_id, nickname, password } = req.body;
 
     //validation(검사) 이 맨 위에 있어야 함
-    connection.query(`SELECT * FROM user WHERE user_id = '${id}'`, function (error, results) {
-        if (error) {
-            console.log(error);
-            res.status(500).json({
-                message: error.message,
-            });
-            return;
-        }
-
-        if (results.length >= 1) {
-            res.status(403).json({
-                code: 1,
-                message: '이미 존재하는 아이디입니다.',
-            });
-            return;
-        }
-
-        connection.query(`SELECT * FROM user WHERE nickname = '${nickname}'`, function (error, results) {
+    connection.query(
+        `
+        SELECT 
+            * 
+        FROM user 
+        WHERE 1 = 1
+        AND user_id = '${user_id}'
+        `,
+        function (error, results) {
             if (error) {
                 console.log(error);
                 res.status(500).json({
@@ -101,99 +39,381 @@ app.post('/join', (req, res) => {
 
             if (results.length >= 1) {
                 res.status(403).json({
-                    code: 2,
-                    message: '이미 존재하는 닉네임입니다.',
+                    code: 1,
+                    message: '이미 존재하는 아이디입니다.',
                 });
                 return;
             }
-        });
 
-        //nickname까지 검사한 후 맨 마지막에 넣는 query
-        connection.query(
-            `INSERT INTO user(user_id, password, nickname) VALUES('${id}', SHA2('${password}', 512), '${nickname}')`,
-            function (error, results) {
-                if (error) {
-                    console.log(error);
-                    res.status(500).json({
-                        message: error.message,
-                    });
-                    return;
+            connection.query(
+                `
+                SELECT 
+                    * 
+                FROM user 
+                WHERE 1 = 1
+                AND nickname = '${nickname}'
+                `,
+                function (error, results) {
+                    if (error) {
+                        console.log(error);
+                        res.status(500).json({
+                            message: error.message,
+                        });
+                        return;
+                    }
+
+                    if (results.length >= 1) {
+                        res.status(403).json({
+                            code: 2,
+                            message: '이미 존재하는 닉네임입니다.',
+                        });
+                        return;
+                    }
                 }
-                res.status(200).json(results);
-            }
-        );
-    });
-});
+            );
 
-app.post('/rank', (req, res) => {
-    const { finalScore, currentNickname } = req.body;
-
-    connection.query(
-        `INSERT INTO RankHistory(score, nickname) VALUES('${finalScore}','${currentNickname}')`,
-        function (error, results) {
-            if (error) {
-                console.log(error);
-                res.status(500).json({
-                    erorrMessage: error,
-                });
-                return;
-            }
+            //nickname까지 검사한 후 맨 마지막에 넣는 query
+            connection.query(
+                `
+                INSERT INTO 
+                    user(user_id, password, nickname) 
+                VALUES('${user_id}', SHA2('${password}', 512), '${nickname}')
+                `,
+                function (error, results) {
+                    if (error) {
+                        console.log(error);
+                        res.status(500).json({
+                            message: error.message,
+                        });
+                        return;
+                    }
+                    connection.query(
+                        `SELECT 
+                            id 
+                        FROM user 
+                        WHERE 1 = 1
+                        AND user_id = '${user_id}'
+                        `,
+                        (error, result) => {
+                            if (error) {
+                                console.log(error);
+                                return;
+                            }
+                            // console.log(result);
+                            res.status(200).json(result);
+                        }
+                    );
+                }
+            );
         }
     );
-    connection.query(`SELECT * FROM RankHistory`, function (error, results) {
-        if (error) {
-            console.log(error);
-            return;
-        }
-        const scoreList = [];
-        const nicknameList = [];
-        const highestScore = [];
-        const highestScoreNicknameList = [];
-        const secondHighestScore = [];
-        const secondHighestScoreNicknameList = [];
-        const thirdHighestScore = [];
-        const thirdHighestScoreNicknameList = [];
+});
 
-        function getRankedList(score, nicknamelst, scorelst) {
-            for (i = 0; i < results.length; i++) {
-                if (scorelst[i] == Math.max(...scorelst)) {
-                    if (score.includes(scorelst[i])) {
-                    } else {
-                        score.push(scorelst[i]);
+// 로그인
+app.post('/auth', (req, res) => {
+    const { user_id, password } = req.body;
+    connection.query(
+        `
+        SELECT 
+            * 
+        FROM user 
+        WHERE 1 = 1
+        AND user_id = '${user_id}'
+        `,
+        function (error, results) {
+            if (results.length == 0) {
+                res.status(403).json({
+                    code: 1,
+                    message: '아이디를 확인해 주세요.',
+                });
+                return;
+            }
+            connection.query(
+                `
+                SELECT 
+                    * 
+                FROM user 
+                WHERE 1 = 1
+                AND user_id = '${user_id}' 
+                and password = SHA2('${password}',512)
+                `,
+                function (error, results) {
+                    if (results.length == 0) {
+                        res.status(403).json({
+                            code: 2,
+                            message: '비밀번호를 확인해 주세요.',
+                        });
+                        return;
                     }
+                    connection.query(
+                        `
+                    SELECT 
+                        id, nickname 
+                    FROM user 
+                    WHERE 1 = 1
+                    AND user_id = '${user_id}'
+                    `,
+                        (error, results) => {
+                            if (error) {
+                                console.log(error);
+                                return;
+                            }
+                            res.status(200).json(results);
+                        }
+                    );
+                }
+            );
+        }
+    );
+});
 
-                    if (nicknamelst.includes(nicknameList[i])) {
-                    } else {
-                        nicknamelst.push(nicknameList[i]);
+// 게임 시작 후 티켓 발행
+app.post('/ticket', (req, res) => {
+    const { id } = req.body;
+    connection.query(
+        `
+        INSERT INTO 
+            GAME_TICKET(user_id) 
+        VALUES('${id}')
+        `,
+        (error, results) => {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            res.status(200).json(results);
+        }
+    );
+});
+
+app.post('/game', (req, res) => {
+    const { id, answer, guess, nickname } = req.body;
+    const getScore = (guess, answer) => {
+        let _strike = 0;
+        let _ball = 0;
+        let _out = 0;
+
+        for (let answerIndex = 0; answerIndex < guess.length; answerIndex++) {
+            if (answer[answerIndex] === guess[answerIndex]) {
+                _strike += 1;
+            } else if (guess.includes(answer[answerIndex])) {
+                _ball += 1;
+            } else {
+                _out += 1;
+            }
+        }
+
+        return { strike: _strike, ball: _ball, out: _out };
+    };
+    const { strike, ball, out } = getScore(guess, answer);
+
+    // game_round_history 와 game_ticket 연결
+    connection.query(
+        `
+        SELECT 
+            id 
+        FROM GAME_TICKET 
+        WHERE user_id = '${id}'
+        ORDER BY created_at DESC 
+        LIMIT 1
+        `,
+        (error, result) => {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            connection.query(
+                `
+                INSERT INTO 
+                GAME_ROUND_HISTORY(guess, user_id, strike_count, ball_count, out_count, game_ticket_id) 
+                VALUES('${guess}', '${id}', '${strike}', '${ball}', '${out}','${result[0].id}')
+                `,
+                (error, results) => {
+                    if (error) {
+                        console.log(error);
+                        return;
                     }
                 }
+            );
+
+            connection.query(
+                `
+                SELECT 
+                    guess, strike_count, ball_count, out_count
+                FROM GAME_ROUND_HISTORY 
+                WHERE 1 = 1
+                AND game_ticket_id = '${result[0].id}'
+                ORDER BY id DESC 
+                LIMIT 1
+                `,
+                (error, results) => {
+                    if (error) {
+                        console.log(error);
+                        return;
+                    }
+                    res.status(200).json(results);
+                }
+            );
+        }
+    );
+});
+
+// 게임의 정답을 맞추었을 때 update : score, deleted_at 후에 table이 생성된 시간과 삭제된 시간을 select
+app.post('/gameEnd', (req, res) => {
+    const { id, score } = req.body;
+    connection.query(
+        `
+        UPDATE GAME_TICKET 
+        SET 
+            score = ${score}, 
+            deleted_at = NOW() 
+        WHERE 1 = 1 
+        AND user_id = '${id}' 
+        ORDER BY created_at DESC
+        LIMIT 1
+        `,
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return;
             }
-            return score, nicknamelst;
+            connection.query(
+                `
+                SELECT 
+                    RUN_TIME
+                FROM 
+                (
+                    SELECT
+                    *,
+                    TIME_TO_SEC(TIMEDIFF(deleted_at, created_at)) AS RUN_TIME
+                    FROM
+                    GAME_TICKET
+                ) AS GAME_TICKET_WITH_RUN_TIME
+                WHERE 1 = 1 
+                AND user_id = '${id}'
+                ORDER BY created_at DESC
+                LIMIT 1
+                `,
+                (error, result) => {
+                    if (error) {
+                        console.log(error);
+                        return;
+                    }
+                    res.status(200).json(result);
+                }
+            );
         }
+    );
+});
 
-        // scoreList에 모든 score 정보를 담고, nicknameList에 모든 nickname 정보를 담는다.
-        for (let i = 0; i < results.length; i++) {
-            scoreList.push(results[i].score);
-            nicknameList.push(results[i].nickname);
+// 명예의 전당
+app.post('/rank', (req, res) => {
+    let highestScore;
+    let secondHighestScore;
+    let thirdHighestScore;
+    let highestScoreNicknameList = [];
+    let secondHighestScoreNicknameList = [];
+    let thirdHighestScoreNicknameList = [];
+    // 1등 리스트, 스코어 추출
+    connection.query(
+        `
+        SELECT 
+            * 
+        FROM (
+            SELECT 
+                score, nickname, ROW_NUMBER() 
+            OVER (PARTITION BY gt.user_id ORDER BY score DESC ) AS RankNo 
+            FROM (GAME_TICKET AS gt JOIN USER as u ON gt.user_id = u.id )
+        ) top_rank
+        WHERE 1 = 1
+        AND RankNo = 1 
+        AND score = (SELECT MAX(score) FROM GAME_TICKET)
+        `,
+        (error, results) => {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            if (results && results.length > 0) {
+                highestScore = results[0].score;
+                for (i = 0; i < results.length; i++) {
+                    highestScoreNicknameList.push(results[i].nickname);
+                }
+            }
+
+            // 2등 리스트, 스코어 추출
+            connection.query(
+                `
+                SELECT * FROM (
+                    SELECT 
+                        score, nickname, ROW_NUMBER() 
+                    OVER (PARTITION BY gt.user_id ORDER BY score DESC ) AS RankNo 
+                    FROM (GAME_TICKET AS gt JOIN USER as u ON gt.user_id = u.id )
+                ) top_rank
+                WHERE 1 = 1
+                AND RankNo = 1 
+                AND score = (SELECT MAX(score) FROM GAME_TICKET WHERE score < (SELECT MAX(score) FROM GAME_TICKET))
+                `,
+                (error, results) => {
+                    if (error) {
+                        console.log(error);
+                        return;
+                    }
+                    if (results && results.length > 0) {
+                        secondHighestScore = results[0].score;
+                        for (i = 0; i < results.length; i++) {
+                            secondHighestScoreNicknameList.push(results[i].nickname);
+                        }
+                    }
+
+                    // 3등 리스트, 스코어 추출
+                    connection.query(
+                        `
+                        SELECT * FROM (
+                            SELECT 
+                                score, nickname, ROW_NUMBER() 
+                            OVER (PARTITION BY gt.user_id ORDER BY score DESC ) AS RankNo 
+                            FROM (GAME_TICKET AS gt JOIN USER as u ON gt.user_id = u.id )
+                        ) top_rank
+                        WHERE 1 = 1
+                        AND RankNo = 1 
+                        AND score = (SELECT MAX(score) FROM GAME_TICKET WHERE score < ((SELECT MAX(score) FROM GAME_TICKET WHERE score < (SELECT MAX(score) FROM GAME_TICKET))))
+                        `,
+                        (error, results) => {
+                            if (error) {
+                                console.log(error);
+                                return;
+                            }
+                            console.log(results);
+                            if (results && results.length > 0) {
+                                thirdHighestScore = results[0].score;
+                                for (i = 0; i < results.length; i++) {
+                                    thirdHighestScoreNicknameList.push(results[i].nickname);
+                                }
+                            } else {
+                            }
+                            // // 스코어 랭킹에 겹치는 이름이 없도록 filtering
+                            // secondHighestScoreNicknameList = secondHighestScoreNicknameList.filter(
+                            //     (n) => !highestScoreNicknameList.includes(n)
+                            // );
+                            // thirdHighestScoreNicknameList = thirdHighestScoreNicknameList.filter(
+                            //     (n) =>
+                            //         !highestScoreNicknameList.includes(n) && !secondHighestScoreNicknameList.includes(n)
+                            // );
+                            res.status(200).json({
+                                highestScore: highestScore,
+                                secondHighestScore: secondHighestScore,
+                                thirdHighestScore: thirdHighestScore,
+                                highestScoreNicknameList: highestScoreNicknameList,
+                                secondHighestScoreNicknameList: secondHighestScoreNicknameList,
+                                thirdHighestScoreNicknameList: thirdHighestScoreNicknameList,
+                            });
+                        }
+                    );
+                }
+            );
         }
-
-        getRankedList(highestScore, highestScoreNicknameList, scoreList);
-        const highestScoreRemovedList = scoreList.filter((score) => score != highestScore[0]);
-
-        getRankedList(secondHighestScore, secondHighestScoreNicknameList, highestScoreRemovedList);
-        const secondHighestScoreRemovedList = highestScoreRemovedList.filter((score) => score != secondHighestScore[0]);
-
-        getRankedList(thirdHighestScore, thirdHighestScoreNicknameList, secondHighestScoreRemovedList);
-
-        res.status(200).json({
-            highestScore: highestScore,
-            highestScoreNicknameList: highestScoreNicknameList,
-            secondHighestScore: secondHighestScore,
-            secondHighestScoreNicknameList: secondHighestScoreNicknameList,
-            thirdHighestScore: thirdHighestScore,
-            thirdHighestScoreNicknameList: thirdHighestScoreNicknameList,
-        });
-    });
+    );
 });
 
 app.listen(port, () => {
